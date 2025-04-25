ef0eb3cd5aa0e0d69f0e0cab364f1f33e6a9fdf1
insubu <tkisid2@gmail.com>
2025-04-26 08:38:07 +0900

    Update sample1.js

--- sample1.js
+++ sample1.js
@@ -41,23 +41,35 @@ begin
 end;
 
 DFM File
-object Form1: TForm1
-  Left = 1247
-  Top = 496
-  Caption = 'SetParameterGenerator'
-  ClientHeight = 182
-  ClientWidth = 284
-  object rdgExistSection: TRadioGroup
-    Left = 8
-    Height = 60
-    Hint = '既に存在するSetParameterGeneratorセクションの扱いを設定します'
-    Top = 8
-    Width = 152
-    Caption = '既存のセクション'
-    ItemIndex = 0
-    Items.Strings = (
-      'コメントアウトする'
-      '上書きする'
-    )
-    TabOrder = 1
-  end
+object lblDefaultType: TLabel
+Left = 16
+Height = 12
+Top = 79
+Width = 64
+Caption = 'デフォルト型'
+ParentColor = False
+end
+object cmbDefaultType: TComboBox
+Left = 102
+Height = 26
+Hint = '出力するデフォルト型を設定します'
+Top = 72
+Width = 100
+ItemHeight = 12
+ItemIndex = 0
+Items.Strings = (
+  'String'
+  'Short'
+  'Integer'
+  'Currency'
+  'Float'
+  'Date'
+  'Time'
+  'DateTime'
+  'Boolean'
+  'BCD'
+)
+Style = csDropDownList
+TabOrder = 2
+Text = 'String'
+end
