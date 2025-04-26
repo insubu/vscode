80381fbac9ef683f20eac011121c7adc43887d01
insubu <tkisid2@gmail.com>
2025-04-26 15:11:01 +0900

    Update sample1.js

--- sample1.js
+++ sample1.js
@@ -1,15 +1,49 @@
+using System;
+using System.Windows.Forms;
 using Microsoft.Web.WebView2.WinForms;
+using Microsoft.Web.WebView2.Core;
 
-public partial class MainForm : Form
+class WebView2FormApp
 {
-    public MainForm()
+    [STAThread]
+    static void Main()
     {
-        InitializeComponent();
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
 
-        var webView = new WebView2();
-        webView.Dock = DockStyle.Fill;
-        this.Controls.Add(webView);
+        // Create the form
+        Form mainForm = new Form
+        {
+            Text = "WebView2 Sample",
+            Width = 1000,
+            Height = 700
+        };
 
-        webView.Source = new Uri("https://www.microsoft.com");
+        // Create the WebView2 control
+        var webView = new WebView2
+        {
+            Dock = DockStyle.Fill
+        };
+
+        // Add the WebView2 to the form
+        mainForm.Controls.Add(webView);
+
+        // Initialize WebView2
+        webView.CoreWebView2InitializationCompleted += (s, e) =>
+        {
+            if (e.IsSuccess)
+            {
+                webView.CoreWebView2.Navigate("https://www.example.com");
+            }
+            else
+            {
+                MessageBox.Show("WebView2 initialization failed: " + e.InitializationException.Message);
+            }
+        };
+
+        webView.EnsureCoreWebView2Async();
+
+        // Run the form
+        Application.Run(mainForm);
     }
 }
